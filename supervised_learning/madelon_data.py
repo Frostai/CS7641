eb44793fc44ee53470fae6a1f2e14b9b32a3b039
tristani <frostaaii@gmail.com>
2021-09-19 23:44:13 -0400

    add dataset, add multiple methods. TODO: track learning rates and make plots

--- supervised_learning/madelon_data.py
+++ supervised_learning/madelon_data.py
@@ -10,39 +10,34 @@ from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-VERBOSE=True
+VERBOSE=False
 
 madelon = pd.read_csv('../datasets/madelon/madelon.csv', sep=',')
 
 X = madelon.values[:, :-1]
 y = madelon.values[:,-1]
-X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=1)
 
 
 # DNN
 
-dnn = MLPClassifier(hidden_layer_sizes=24, random_state=1, max_iter=10000, tol=0.00001, n_iter_no_change=100, verbose=VERBOSE)
-dnn.fit(X_train, y_train)
-# dnn.predict_proba(X_test[:1])
-
-print('\nPredict DNN:\n')
-# print(dnn.predict(X_test))
-# print('Accuracy Train: {}'.format(accuracy_score(y_train, dnn.predict(X_train))))
-# print('Accuracy Test: {}'.format(accuracy_score(y_test, dnn.predict(X_test))))
-
-print('Train data Score: ', dnn.score(X_train, y_train))
-print('Test data Score', dnn.score(X_test, y_test))
+# dnn = MLPClassifier(hidden_layer_sizes=(12,), random_state=1, max_iter=10000, tol=0.0001, n_iter_no_change=100, verbose=VERBOSE)
+# dnn.fit(X_train, y_train)
+# print('\nPredict DNN:\n')
+# # print(dnn.predict(X_test))
+# print('Train data Score: ', dnn.score(X_train, y_train))
+# print('Test data Score', dnn.score(X_test, y_test))
 
 # KNN
-knn = KNeighborsClassifier(n_neighbors=25, leaf_size=5, n_jobs=-1)
-print('\nPredict KNN:\n')
+knn = KNeighborsClassifier(n_neighbors=30, leaf_size=10, weights='distance', n_jobs=-1)
+print('\nPredict KNN:')
 knn.fit(X_train, y_train)
 # print(knn.predict(X_test))
 print('Train data Score: ', knn.score(X_train, y_train))
 print('Test data Score', knn.score(X_test, y_test))
 
 # DecisionTree
-print('\nDecision Trees:\n')
+print('\nDecision Trees:')
 tree = DecisionTreeClassifier(random_state=0, min_samples_leaf=5)
 tree.fit(X_train, y_train)
 # print(tree.predict(X_test))
@@ -52,7 +47,7 @@ print('Test data Score', tree.score(X_test, y_test))
 
 
 # Forest
-print('\nRandom Forests:\n')
+print('\nRandom Forests:')
 forest = RandomForestClassifier(random_state=0, min_samples_leaf=12)
 forest.fit(X_train, y_train)
 # print(forest.predict(X_test))
@@ -61,8 +56,9 @@ print('Test data Score', forest.score(X_test, y_test))
 
 
 # SVM
-# svms = svm.SVC()
-# svms.fit(X_train, y_train)
+print('\nSVM:')
+svms = svm.SVC()
+svms.fit(X_train, y_train)
 # print(svms.predict(X_test))
-# print('Train data Score: ', svms.score(X_train, y_train))
-# print('Test data Score', svms.score(X_test, y_test))
+print('Train data Score: ', svms.score(X_train, y_train))
+print('Test data Score', svms.score(X_test, y_test))
